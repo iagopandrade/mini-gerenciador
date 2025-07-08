ebef93ac8b7c81715517b01bfdd0d65da5c29523
iagopandrade <iagopinheirodeandrade@gmail.com>
2025-07-08 18:07:52 -0300

    Create commandtext.c

---
+++ commandtext.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "utils.h"
+
+int CommandText(char cmdtext[], int tam) {
+	char *arg;
+	// Teste
+	int retorno;
+	
+	// Remove o "\n" que a função fgets() colocou
+	cmdtext[strcspn(cmdtext, "\n")] = 0;
+	
+	// Separa o argumento do comando (Comando: read; argumento: arquivo.txt)
+    arg = strtok(cmdtext, " ");	
+	
+	if (strcmp(cmdtext, "ler") == 0) {
+		arg = strtok(NULL, " ");
+		
+		retorno = ReadFile(arg, sizeof(arg)); 
+		if(retorno == 1) {
+			printf("DEBUG :retorno %d\n", retorno);
+			printf("Digite: read [nomedoarquivo.txt]\n");		
+			//file = fopen(arg, "r");
+			return 1;
+		} if (retorno == 2) {
+			printf("'%s' não existe.\n", arg);		
+			//file = fopen(arg, "r");
+			return 1;
+		} 
+		
+		//debug
+		printf("\n\n\nAbriu?\n\n\n");
+		return 0; 	
+	} 
+	
+	if (strcmp(cmdtext, "sair") == 0) {		
+		Exit();  
+	} 	
+	
+	else {
+		printf("'%s' comando não encontrado\n> ", cmdtext);
+	}
+
+}
