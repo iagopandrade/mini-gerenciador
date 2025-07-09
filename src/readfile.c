ec4a22f4fbab0004aaf763a38d8032af76016c80
iagopandrade <iagopinheirodeandrade@gmail.com>
2025-07-09 04:51:24 -0300

    Refactor ReadFile function for clarity and cleanup
    Verificações condicionais simplificadas e remoção de variáveis ​​não utilizadas e código comentado em ReadFile. Formatação de saída aprimorada e leitura de conteúdo de arquivo simplificada.

--- src/readfile.c
+++ src/readfile.c
@@ -10,28 +10,17 @@ int ReadFile(char nameFile[], int tam) {
 	
 	// Variáveil para armazenar o conteúdo dos arquivos
 	char fileContent[100];
-	char *ReturnContent = fileContent;
 	
 	// Verifica se o usuário digitou o nome do arquivo
-	if (nameFile == NULL) 
-		return 1; // Retorna 1, se o usuário não digitou o nome
+	if (nameFile == NULL) return 1; // Retorna 1, se o usuário não digitou o nome
 		
 	// Verifica se o arquivo existe
-	if (file == NULL) 
-		return 2; // Retorna 2, se o arquivo não existir
-
-	// Precisa retornar o fileContet para o comparador de cmds		
-	printf("O arquivo'%s' foi aberto\n", nameFile);	
-	fscanf(file, "%s", fileContent);
+	if (file == NULL) return 2; // Retorna 2, se o arquivo não existir
+		
+	printf("Arquivo '%s'\n", nameFile);
+	printf("Conteúdo: ");
+	fscanf(file, "%s\n", fileContent);
 	printf("%s\n", fileContent);		
-	return 0;
+	return 0; 
 }
 
-// bugada, vou resolver depois
-// Condição para verificar formatação (Está bugada)
-		/*
-		if(!strcspn(arg, "'!@#$%") == NULL) {
-			printf("Formatação inválida\n");
-			return 1;
-		}
-		*/
