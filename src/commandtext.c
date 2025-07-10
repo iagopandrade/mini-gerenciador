5c09a307c7fb217f4489389a7a2214d79b663862
iagopandrade <iagopinheirodeandrade@gmail.com>
2025-07-10 03:33:52 -0300

    Add 'cls' command and improve command handling
    Introduzido suporte para o comando "cls" para limpar a tela. Estrutura de código aprimorada, documentação de funções adicionada e feedback do usuário aprimorado para comandos e argumentos inválidos.

--- src/commandtext.c
+++ src/commandtext.c
@@ -1,45 +1,58 @@
 #include <stdio.h>
 #include <string.h>
-
 #include "utils.h"
 
-int CommandText(char cmdtext[], int tam) {
+/**
+ * @brief Compara comandos.
+ *
+ * Esta função compara uma string com um comando interno.
+ *
+ * @param cmdtext Comando digitado pelo usuário.
+ * @param tam Tamanho do array cmdtext.
+ *
+ * @return int Retorna 1 se o comando for executado.
+ *
+ */
+int CommandText(char cmdtext[], int tam)
+{
 	char *arg = NULL;
-	
+
+	// Variável para armazenar o retorno da função
+	int functionResult = NULL;
+
 	// Remove o "\n" que a função fgets() colocou
 	cmdtext[strcspn(cmdtext, "\n")] = 0;
-	
+
 	// Separa o argumento do comando (Comando: read; argumento: arquivo.txt)
-    arg = strtok(cmdtext, " ");	
-	
+	arg = strtok(cmdtext, " ");
+
 	if (strcmp(cmdtext, "ler") == 0) {
 		arg = strtok(NULL, " ");
-		
-		// Variável para armazenar o retorno da função
-		int functionResult = NULL;
 
-		functionResult = ReadFile(arg, sizeof(arg)); 
-		
-		if(functionResult != 0) {
-			if(functionResult == 1) {
-				printf("Digite: ler [nomedoarquivo.txt]\n");		
-				//file = fopen(arg, "r");		
-			} if (functionResult > 1) {
-				printf("O arquivo '%s' não existe.\n", arg);		
-				//file = fopen(arg, "r");
-			}			
-		} // else printf("O arquivo '%s' foi aberto.\n", arg);	 	
-	
-	return 1;	
-	} 
-	
-	if (strcmp(cmdtext, "sair") == 0) {		
-		Exit();  
-	} 	
-	
-	else {
-		printf("O comando '%s' não foi encontrado\n> ", cmdtext);
+		functionResult = ReadFile(arg, sizeof(arg));
+
+		if (functionResult != 0) {
+			if (functionResult == 1) {
+				printf("Digite: ler[nomedoarquivo.txt]\n > ");
+			}
+
+			if (functionResult > 1) {
+				printf("O arquivo '%s' não existe.\n > ", arg);
+			}
+		}
+		return 1;
+	}
+
+	if (strcmp(cmdtext, "sair") == 0) {
+		Exit();
+	}
+
+	if (strcmp(cmdtext, "cls") == 0) {
+		system("cls");
+		return 1;
 	}
 	
-	return 0;
+	else {
+		printf("O comando '%s' não foi encontrado\n > ", cmdtext);
+	}
 }
