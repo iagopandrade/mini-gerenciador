7e50d4f2e68a34d349c2c2c6a84deff850c99c5f
iagopandrade <iagopinheirodeandrade@gmail.com>
2025-07-11 21:03:24 -0300

    Refactor readfile function and update formatting
    A função foi renomeada de read_file para readfile e sua formatação foi atualizada para melhorar a legibilidade. A formatação de saída para a exibição do conteúdo do arquivo foi aprimorada, incluindo numeração de linhas e linhas separadoras. Um comentário de cabeçalho em formato ASCII foi adicionado no topo do arquivo.

--- src/readfile.c
+++ src/readfile.c
@@ -1,3 +1,17 @@
+/***
+ *                                         /$$  /$$$$$$  /$$ /$$          
+ *                                        | $$ /$$__  $$|__/| $$          
+ *      /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$$| $$  \__/ /$$| $$  /$$$$$$ 
+ *     /$$__  $$ /$$__  $$ |____  $$ /$$__  $$| $$$$    | $$| $$ /$$__  $$
+ *    | $$  \__/| $$$$$$$$  /$$$$$$$| $$  | $$| $$_/    | $$| $$| $$$$$$$$
+ *    | $$      | $$_____/ /$$__  $$| $$  | $$| $$      | $$| $$| $$_____/
+ *    | $$      |  $$$$$$$|  $$$$$$$|  $$$$$$$| $$      | $$| $$|  $$$$$$$
+ *    |__/       \_______/ \_______/ \_______/|__/      |__/|__/ \_______/
+ *                                                                        
+ *                                                                        
+ *                                                                        
+ */
+ 
 #include <stdio.h>
 #include <string.h>
 #include "utils.h"
@@ -11,35 +25,44 @@
  * @return int Retorna 1 se não receber parâmetro, 2 caso o parâmetro seja inválido.
  *
  */
-int read_file ( char nameFile[], int tam ) {
+int
+readfile ( char nameFile[], int tam ) 
+{
 	FILE *file = fopen(nameFile, "r");
 	
-	if (nameFile == NULL) return 1;
-
-	if (file == NULL) return 2;
-	else {
+	if (nameFile == NULL)
+		return 1;
+	
+	if (file == NULL) 
+		return 2;
+		
+	else 
+	{
 		char fileContent;
 		
 		printf("\n");
 		printf("Arquivo '%s'\n", nameFile);
-		printf("\t----------------------------------------------------------------------------------------------------------------\n");
+		printf("\t-----------------------\n");
 		int i = 1;
 		printf("\t%d. ", i);
-		while ((fileContent = fgetc(file)) != EOF) {
-			if (fileContent == '\n') {
-				i++;
-				fileContent = 0;
-				printf("\n\t%d. ", i);			
+		
+		while ((fileContent = fgetc(file)) != EOF) 
+			{
+				if (fileContent == '\n') 
+				{
+					i++;
+					fileContent = 0;
+					printf("\n\t%d. ", i);			
+				}
+				putchar(fileContent);
 			}
-			putchar(fileContent);
-		}
 
 		fclose(file);
 
-		printf("\n\t----------------------------------------------------------------------------------------------------------------\n");
+		printf("\n\t-------------------\n");
 		printf("%d linhas\n", i); i = 0;
 		printf("Arquivo '%s' foi fechado", nameFile);
-		printf("\n\n > ");
+		printf("\n\n >");
 	}
 	return 0;
 }
