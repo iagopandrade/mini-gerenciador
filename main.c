4df0de4df16d4e7fa3c46af08c28f6d313d738ea
iagopandrade <iagopinheirodeandrade@gmail.com>
2025-07-11 21:02:52 -0300

    Update main menu and locale settings in main.c
    Adicionado cabeçalho de arte ASCII e exibição aprimorada do menu de comandos. Configuração de localidade alterada para "ptb". Descrições de comandos atualizadas e marcadores de posição adicionados para novos comandos. Loop de manipulação de comandos refatorado e erro de digitação no nome da função corrigido.

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+/***
+ *                  $$\           $$\                                                                     $$\                 $$\                     
+ *                  \__|          \__|                                                                    \__|                $$ |                    
+ *    $$$$$$\$$$$\  $$\ $$$$$$$\  $$\          $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\  $$$$$$$\   $$$$$$$\ $$\  $$$$$$\   $$$$$$$ | $$$$$$\   $$$$$$\  
+ *    $$  _$$  _$$\ $$ |$$  __$$\ $$ |$$$$$$\ $$  __$$\ $$  __$$\ $$  __$$\ $$  __$$\ $$  __$$\ $$  _____|$$ | \____$$\ $$  __$$ |$$  __$$\ $$  __$$\ 
+ *    $$ / $$ / $$ |$$ |$$ |  $$ |$$ |\______|$$ /  $$ |$$$$$$$$ |$$ |  \__|$$$$$$$$ |$$ |  $$ |$$ /      $$ | $$$$$$$ |$$ /  $$ |$$ /  $$ |$$ |  \__|
+ *    $$ | $$ | $$ |$$ |$$ |  $$ |$$ |        $$ |  $$ |$$   ____|$$ |      $$   ____|$$ |  $$ |$$ |      $$ |$$  __$$ |$$ |  $$ |$$ |  $$ |$$ |      
+ *    $$ | $$ | $$ |$$ |$$ |  $$ |$$ |        \$$$$$$$ |\$$$$$$$\ $$ |      \$$$$$$$\ $$ |  $$ |\$$$$$$$\ $$ |\$$$$$$$ |\$$$$$$$ |\$$$$$$  |$$ |      
+ *    \__| \__| \__|\__|\__|  \__|\__|         \____$$ | \_______|\__|       \_______|\__|  \__| \_______|\__| \_______| \_______| \______/ \__|      
+ *                                            $$\   $$ |                                                                                              
+ *                                            \$$$$$$  |                                                                                              
+ *                                             \______/                                                                                               
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,23 +20,40 @@
 
 int main(int argc, char *argv[])
 {
-	setlocale(LC_ALL, "Portuguese");
-	
-	printf("Gerenciador de Arquivos\n\n");
-	printf("Comandos adicionados:\n");
-	printf("- ler[nome do arquivo]\n");
-	printf("- sair\n");
+	setlocale(LC_ALL, "ptb");
+
+	printf("Minigerenciador de Arquivos");
 	system("cd");
-	printf(" > ");
+	
+	printf("\n");
+	printf("Comandos adicionados:\n");
+	printf("ler");
+	printf("\tExibe o conteúdo de um arquivo.\n");
+	printf("limpar");
+	printf("\tLimpa a tela.\n");
+	printf("sair");
+	printf("\tEncerra o programa.\n");
+	printf("\n");
+	
+	printf("Comandos em desenvolvimento:\n");
+	printf("criar");
+	
+	printf("\n\n >");
 
 	// Cria um laço infinito, saindo apenas com o comando 'sair'
-	while (1) {
+	while (1) 
+	{
 		char cmd[50];
+		
 		// Limpa o buffer do teclado
 		setbuf(stdin, NULL);
 		fgets(cmd, sizeof(cmd), stdin);
 
-		command_text(cmd, sizeof(cmd));
+		commandtext(cmd, sizeof(cmd));
+		
+		int i;
+		for (i = 50; i > 0; i--)
+			cmd[i] = 0;
 	}
 	return 0;
 }
