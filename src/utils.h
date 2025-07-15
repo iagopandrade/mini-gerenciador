36bcc3faeee7a16eb6166210f75664b134fde194
iagopandrade <iagopinheirodeandrade@gmail.com>
2025-07-15 20:12:05 -0300

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -12,19 +12,13 @@
  *                                          
  */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <locale.h>
-
 #ifndef UTILS_H
 #define UTILS_H
 
 int commandtext ( char [], int );
 int readfile ( char [], int );
 int writefile( char [], int );
-
-void printcf( char[], int );
+void fprintc( char[], int );
 int getoption();
 
 #endif
